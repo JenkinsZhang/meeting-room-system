b621605b1007d9842be6ff0ec6a58db65c1928b8
JenkinsZhang <614758656@qq.com>
2020-01-13 22:29:26 +0800

    update files

--- system-backend/registry-server/src/main/java/com/jenkins/common/registryserver/RegistryServerApplication.java
+++ system-backend/registry-server/src/main/java/com/jenkins/common/registryserver/RegistryServerApplication.java
@@ -1,4 +1,4 @@
-package com.jenkins.common.bookingservice.registryserver;
+package com.jenkins.common.registryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- system-backend/system-auth/auth-interface/src/main/java/com/jenkins/common/authcommon/AuthCommonApplication.java
+++
@@ -1,8 +0,0 @@
-package com.jenkins.common.authcommon;
-
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class AuthCommonApplication {
-}
